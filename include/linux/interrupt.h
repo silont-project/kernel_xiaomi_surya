65897d52f265331e31231274c229682eaf700ebd
Thomas Gleixner <tglx@linutronix.de>
2012-01-31 13:01:27 +0100

    genirq: Allow disabling of softirq processing in irq thread context
    The processing of softirqs in irq thread context is a performance gain
    for the non-rt workloads of a system, but it's counterproductive for
    interrupts which are explicitely related to the realtime
    workload. Allow such interrupts to prevent softirq processing in their
    thread context.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: azrim <mirzaspc@gmail.com>

--- include/linux/interrupt.h
+++ include/linux/interrupt.h
@@ -66,6 +66,7 @@
  *                their interrupt handlers.
  * IRQF_PERF_AFFINE - Interrupt is critical to the overall performance of the
  *		      system and should be processed on a big CPU.
+ * IRQF_NO_SOFTIRQ_CALL - Do not process softirqs in the irq thread context (RT)
  */
 #define IRQF_SHARED		0x00000080
 #define IRQF_PROBE_SHARED	0x00000100
@@ -80,7 +81,7 @@
 #define IRQF_EARLY_RESUME	0x00020000
 #define IRQF_COND_SUSPEND	0x00040000
 #define IRQF_PERF_AFFINE	0x00080000
-
+#define IRQF_NO_SOFTIRQ_CALL	0x00200000
 #define IRQF_TIMER		(__IRQF_TIMER | IRQF_NO_SUSPEND | IRQF_NO_THREAD)
 
 /*
