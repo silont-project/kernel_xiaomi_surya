5db3d51df0d4981d5cc919b867f84ab59148302f
李俊 <lijun@longcheer.com>
2020-07-21 15:22:17 +0800

    Revert "[M700][camera][HTH-105811]超清像素拍照，偶现相机故障 [rootcause]:update  algo&bin [solution]: update algo&bin [sideeffect]:no effect [others]:no effect"
    This reverts commit 9c781cfbf274713908db992d93e30e3d3da96711.
    
    Change-Id: I63338c5aafd562f26f971a1e4498885b3b0df799
    Signed-off-by: Hiitagiii <mirzaspc@gmail.com>

--- drivers/iommu/iova.c
+++ drivers/iommu/iova.c
@@ -442,7 +442,6 @@ retry:
 		flushed_rcache = true;
 		for_each_online_cpu(cpu)
 			free_cpu_cached_iovas(cpu, iovad);
-		free_global_cached_iovas(iovad);
 		goto retry;
 	}
 
@@ -791,13 +790,10 @@ error:
  */
 
 #define IOVA_MAG_SIZE 128
-#define IOVA_MAG_SIZE_LOW (IOVA_MAG_SIZE/2)
-#define CACHE_INDEX_LIMIT 3
 
 struct iova_magazine {
 	unsigned long size;
-	unsigned long max_size;
-	unsigned long *pfns;
+	unsigned long pfns[IOVA_MAG_SIZE];
 };
 
 struct iova_cpu_rcache {
@@ -806,24 +802,13 @@ struct iova_cpu_rcache {
 	struct iova_magazine *prev;
 };
 
-static struct iova_magazine *iova_magazine_alloc(unsigned long index,
-						 gfp_t flags)
+static struct iova_magazine *iova_magazine_alloc(gfp_t flags)
 {
-	struct iova_magazine *mag;
-
-	mag = kzalloc(sizeof(struct iova_magazine), flags);
-	if (index < CACHE_INDEX_LIMIT)
-		mag->max_size = IOVA_MAG_SIZE_LOW;
-	else
-		mag->max_size = IOVA_MAG_SIZE;
-
-	mag->pfns = kcalloc(mag->max_size, sizeof(unsigned long), flags);
-	return mag;
+	return kzalloc(sizeof(struct iova_magazine), flags);
 }
 
 static void iova_magazine_free(struct iova_magazine *mag)
 {
-	kfree(mag->pfns);
 	kfree(mag);
 }
 
@@ -854,7 +839,7 @@ iova_magazine_free_pfns(struct iova_magazine *mag, struct iova_domain *iovad)
 
 static bool iova_magazine_full(struct iova_magazine *mag)
 {
-	return (mag && mag->size == mag->max_size);
+	return (mag && mag->size == IOVA_MAG_SIZE);
 }
 
 static bool iova_magazine_empty(struct iova_magazine *mag)
@@ -897,8 +882,8 @@ static void init_iova_rcaches(struct iova_domain *iovad)
 		for_each_possible_cpu(cpu) {
 			cpu_rcache = per_cpu_ptr(rcache->cpu_rcaches, cpu);
 			spin_lock_init(&cpu_rcache->lock);
-			cpu_rcache->loaded = iova_magazine_alloc(i, GFP_KERNEL);
-			cpu_rcache->prev = iova_magazine_alloc(i, GFP_KERNEL);
+			cpu_rcache->loaded = iova_magazine_alloc(GFP_KERNEL);
+			cpu_rcache->prev = iova_magazine_alloc(GFP_KERNEL);
 		}
 	}
 }
@@ -911,8 +896,7 @@ static void init_iova_rcaches(struct iova_domain *iovad)
  */
 static bool __iova_rcache_insert(struct iova_domain *iovad,
 				 struct iova_rcache *rcache,
-				 unsigned long iova_pfn,
-				 unsigned long log_size)
+				 unsigned long iova_pfn)
 {
 	struct iova_magazine *mag_to_free = NULL;
 	struct iova_cpu_rcache *cpu_rcache;
@@ -928,8 +912,7 @@ static bool __iova_rcache_insert(struct iova_domain *iovad,
 		swap(cpu_rcache->prev, cpu_rcache->loaded);
 		can_insert = true;
 	} else {
-		struct iova_magazine *new_mag =
-			iova_magazine_alloc(log_size, GFP_ATOMIC);
+		struct iova_magazine *new_mag = iova_magazine_alloc(GFP_ATOMIC);
 
 		if (new_mag) {
 			spin_lock(&rcache->lock);
@@ -967,8 +950,7 @@ static bool iova_rcache_insert(struct iova_domain *iovad, unsigned long pfn,
 	if (log_size >= IOVA_RANGE_CACHE_MAX_SIZE)
 		return false;
 
-	return __iova_rcache_insert(iovad, &iovad->rcaches[log_size],
-			pfn, log_size);
+	return __iova_rcache_insert(iovad, &iovad->rcaches[log_size], pfn);
 }
 
 /*
@@ -1088,27 +1070,6 @@ void free_cpu_cached_iovas(unsigned int cpu, struct iova_domain *iovad)
 		iova_magazine_free_pfns(cpu_rcache->loaded, iovad);
 		iova_magazine_free_pfns(cpu_rcache->prev, iovad);
 		spin_unlock_irqrestore(&cpu_rcache->lock, flags);
-	}
-}
-
-/*
- * free all the IOVA ranges of global cache
- */
-void free_global_cached_iovas(struct iova_domain *iovad)
-{
-	struct iova_rcache *rcache;
-	int i, j;
-
-	for (i = 0; i < IOVA_RANGE_CACHE_MAX_SIZE; ++i) {
-		rcache = &iovad->rcaches[i];
-		spin_lock(&rcache->lock);
-		for (j = 0; j < rcache->depot_size; ++j) {
-			iova_magazine_free_pfns(rcache->depot[j], iovad);
-			iova_magazine_free(rcache->depot[j]);
-			rcache->depot[j] = NULL;
-		}
-		rcache->depot_size = 0;
-		spin_unlock(&rcache->lock);
 	}
 }
 
