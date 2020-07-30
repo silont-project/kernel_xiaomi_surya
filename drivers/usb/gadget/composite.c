5a105c80edb89c5ab5bc5ef77b5f43892ffda5be
liwei6 <liwei6@longcheer.com>
2020-07-30 22:35:04 +0800

    [M700][charge][HMI_M700_A01-410]set 500 mA when usb bus in suspend [rootcause]:The limitation is in the bus suspend function when get the suspend event reported.. [solution]:KBA-171218012414 from qcom [sideeffect]:N/A [others]:N/A
    Conflicts:
    	drivers/usb/gadget/composite.c
    
    Change-Id: I6665acbd66986914859d227d7105607e3eb421cd
    Signed-off-by: Volodymyr Zhdanov <wight554@gmail.com>
    Signed-off-by: azrim <mirzaspc@gmail.com>

--- drivers/usb/gadget/composite.c
+++ drivers/usb/gadget/composite.c
@@ -2486,7 +2486,7 @@ void composite_suspend(struct usb_gadget *gadget)
 	spin_unlock_irqrestore(&cdev->lock, flags);
 
 	usb_gadget_set_selfpowered(gadget);
-	usb_gadget_vbus_draw(gadget, 2);
+	usb_gadget_vbus_draw(gadget, 500);    //qcom KBA-171218012414,liwei6 modify,20200730
 }
 
 void composite_resume(struct usb_gadget *gadget)
