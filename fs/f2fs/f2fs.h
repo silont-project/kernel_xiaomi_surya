69c8b4bd24860bb7e6ffa180325def60023e6371
Shachar Raindel <shacharr@gmail.com>
2020-11-28 09:41:24 -0800

    f2fs: Fix deadlock between f2fs_quota_sync and block_operation
    This deadlock is hitting Android users (Pixel 3/3a/4) with Magisk, due
    to frequent umount/mount operations that trigger quota_sync, hitting
    the race. See https://github.com/topjohnwu/Magisk/issues/3171 for
    additional impact discussion.
    
    In commit db6ec53b7e03, we added a semaphore to protect quota flags.
    As part of this commit, we changed f2fs_quota_sync to call
    f2fs_lock_op, in an attempt to prevent an AB/BA type deadlock with
    quota_sem locking in block_operation.  However, rwsem in Linux is not
    recursive. Therefore, the following deadlock can occur:
    
    f2fs_quota_sync
    down_read(cp_rwsem) // f2fs_lock_op
    filemap_fdatawrite
    f2fs_write_data_pages
    ...
                                       block_opertaion
    				   down_write(cp_rwsem) - marks rwsem as
    				                          "writer pending"
    down_read_trylock(cp_rwsem) - fails as there is
                                  a writer pending.
    			      Code keeps on trying,
    			      live-locking the filesystem.
    
    We solve this by creating a new rwsem, used specifically to
    synchronize this case, instead of attempting to reuse an existing
    lock.
    
    Signed-off-by: Shachar Raindel <shacharr@gmail.com>
    
    Fixes: db6ec53b7e03 f2fs: add a rw_sem to cover quota flag changes
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: azrim <mirzaspc@gmail.com>

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -1635,6 +1635,7 @@ struct f2fs_sb_info {
 	struct f2fs_rwsem cp_rwsem;		/* blocking FS operations */
 	struct f2fs_rwsem node_write;		/* locking node writes */
 	struct f2fs_rwsem node_change;	/* locking node change */
+	struct f2fs_rwsem cp_quota_rwsem;    	/* blocking quota sync operations */
 	wait_queue_head_t cp_wait;
 	unsigned long last_time[MAX_TIME];	/* to store time in jiffies */
 	long interval_time[MAX_TIME];		/* to store thresholds */
@@ -2225,6 +2226,7 @@ static inline void f2fs_unlock_all(struct f2fs_sb_info *sbi)
 {
 	f2fs_up_write(&sbi->cp_quota_rwsem);
 	f2fs_up_write(&sbi->cp_rwsem);
+	f2fs_up_write(&sbi->cp_quota_rwsem);
 }
 
 static inline int __get_cp_reason(struct f2fs_sb_info *sbi)
