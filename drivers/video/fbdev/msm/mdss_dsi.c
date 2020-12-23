960eaf7f64df1a712d9075f63a8f37bad2a54d4c
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-23 17:23:00 +0300

    msm: mdss: Mark display-wake kthread as performance critical
    This kthread is responsible for powering on the display, so it needs to
    run as soon as possible to minimize lag when turning the display on.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Alex Winkowski <dereference23@outlook.com>
    Signed-off-by: azrim <mirzaspc@gmail.com>

--- drivers/video/fbdev/msm/mdss_dsi.c
+++ drivers/video/fbdev/msm/mdss_dsi.c
@@ -3861,7 +3861,8 @@ static int mdss_dsi_ctrl_probe(struct platform_device *pdev)
 
 	init_completion(&ctrl_pdata->wake_comp);
 	init_waitqueue_head(&ctrl_pdata->wake_waitq);
-	ctrl_pdata->wake_thread = kthread_run(mdss_dsi_disp_wake_thread,
+	ctrl_pdata->wake_thread =
+		kthread_run_perf_critical(mdss_dsi_disp_wake_thread,
 					      ctrl_pdata, "mdss_display_wake");
 	if (IS_ERR(ctrl_pdata->wake_thread)) {
 		rc = PTR_ERR(ctrl_pdata->wake_thread);
