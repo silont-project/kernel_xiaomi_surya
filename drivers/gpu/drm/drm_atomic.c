a53450635f7a287ffb505145d4ab71c1edf08210
Yaroslav Furman <yaro330@gmail.com>
2021-03-15 01:34:13 +0300

    drm: drm_atomic: add CIB trigger
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: azrim <mirzaspc@gmail.com>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -32,6 +32,7 @@
 #include <drm/drm_print.h>
 #include <linux/pm_qos.h>
 #include <linux/devfreq_boost.h>
+#include <linux/cpu_input_boost.h>
 #include <linux/sync_file.h>
 
 #include "drm_crtc_internal.h"
@@ -2249,8 +2250,10 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 			(arg->flags & DRM_MODE_PAGE_FLIP_EVENT))
 		return -EINVAL;
 
-	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY))
+	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
+		cpu_input_boost_kick();
 		devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
+	}
 
 	drm_modeset_acquire_init(&ctx, 0);
 
