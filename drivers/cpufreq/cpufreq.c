d3ed9c82f328fcb55e2883cb070bf74e827da8e3
Nauval Rizky <enuma.alrizky@gmail.com>
2021-07-28 04:53:45 +0700

    thermal: core: Revise mi thermal helper implementation
    Current mi_thermald only needs sconfig, and board sensor name and temp,
    it also actually can handle freq without cpu_limits anymore, but since
    we're partially using LMH's cooling ops, lets not touch it, and for
    anything other than those required sysfs, we can just remove it.
    
    Also while at it, lets remove unnecessary thermal policy notifiers since
    the freq incompatibility notifier already did the job nicely.
    
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    [azrim: also remove use of undeclared identifier 'CPUFREQ_THERMAL']
    Signed-off-by: azrim <mirzaspc@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -2263,9 +2263,6 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 	blocking_notifier_call_chain(&cpufreq_policy_notifier_list,
 			CPUFREQ_ADJUST, new_policy);
 
-        blocking_notifier_call_chain(&cpufreq_policy_notifier_list,
-			CPUFREQ_THERMAL, new_policy);
-
 	/* adjust if necessary - hardware incompatibility */
 	blocking_notifier_call_chain(&cpufreq_policy_notifier_list,
 			CPUFREQ_INCOMPATIBLE, new_policy);
