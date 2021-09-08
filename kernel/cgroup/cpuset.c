4bd461b26ec1585a9cdf4bd8dbd554a3ca6761a0
azrim <mirzaspc@gmail.com>
2021-09-08 07:44:56 +0000

    kernel/cgroup: uclamp_assist: tune values a bit * Also add display to uclamp tunables
    Signed-off-by: azrim <mirzaspc@gmail.com>

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -2033,10 +2033,11 @@ static void uclamp_set(struct kernfs_open_file *of,
 	static struct ucl_param tgts[] = {
 		{"top-app",    	     	"10", "100", 1, 1},
 		{"foreground", 	     	"0",  "50",  1, 1},
-		{"restricted", 	    	"10", "40",  0, 0},
+		{"restricted", 	    	"0",  "30",  0, 0},
 		{"background", 	     	"20", "100", 0, 0},
-		{"system-background", 	"0",  "40",  0, 0},
-		{"camera-daemon", 	"10", "40",  0, 0},
+		{"system-background", 	"0",  "30",  0, 0},
+		{"camera-daemon", 	"50", "100", 1, 1},
+                {"display",     	"50", "100", 1, 1},
 	};
 
 	for (i = 0; i < ARRAY_SIZE(tgts); i++) {
