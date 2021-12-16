c3cd336e35d7f441c70f362cf75d3dd2204d91f0
John Galt <johngaltfirstrun@gmail.com>
2021-12-16 11:12:35 -0500

    fs/exec: force dex2oat64 on big cluster
    updateengine behavior is to have 4 low priority threads here, let's
    attempt to speed it up slightly
    
    Signed-off-by: azrim <mirzaspc@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -78,6 +78,7 @@ static LIST_HEAD(formats);
 static DEFINE_RWLOCK(binfmt_lock);
 
 #define HWCOMPOSER_BIN_PREFIX "/vendor/bin/hw/android.hardware.graphics.composer"
+#define DEX2OAT64_BIN_PREFIX "/apex/com.android.art/bin/dex2oat64"
 #define ZYGOTE32_BIN "/system/bin/app_process32"
 #define ZYGOTE64_BIN "/system/bin/app_process64"
 static struct signal_struct *zygote32_sig;
@@ -1847,6 +1848,12 @@ static int do_execveat_common(int fd, struct filename *filename,
 					   HWCOMPOSER_BIN_PREFIX,
 					   strlen(HWCOMPOSER_BIN_PREFIX)))) {
 			current->flags |= PF_PERF_CRITICAL;
+			set_cpus_allowed_ptr(current, cpu_perf_mask);
+		}
+		else if (unlikely(!strncmp(filename->name,
+					   DEX2OAT64_BIN_PREFIX,
+					   strlen(DEX2OAT64_BIN_PREFIX)))) {
+			current->pc_flags |= PC_PERF_AFFINE;
 			set_cpus_allowed_ptr(current, cpu_perf_mask);
 		}
 		else if (unlikely(!strcmp(filename->name, ZYGOTE32_BIN))) {
