fc7ed410b7f1031e634bb6703b594b0eebbf60be
John Galt <johngaltfirstrun@gmail.com>
2022-03-04 22:26:03 -0500

    fs/exec: update force affining
    Drop dex2oat hack, better ways of accomplishing in userspace.
    
    Affine all vendor.qti.hardware services to big cluster.
    
    Signed-off-by: azrim <mirzaspc@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -78,7 +78,7 @@ static LIST_HEAD(formats);
 static DEFINE_RWLOCK(binfmt_lock);
 
 #define HWCOMPOSER_BIN_PREFIX "/vendor/bin/hw/android.hardware.graphics.composer"
-#define DEX2OAT64_BIN_PREFIX "/apex/com.android.art/bin/dex2oat64"
+#define QTIHW_BIN_PREFIX "/vendor/bin/hw/vendor.qti.hardware"
 #define ZYGOTE32_BIN "/system/bin/app_process32"
 #define ZYGOTE64_BIN "/system/bin/app_process64"
 static struct signal_struct *zygote32_sig;
@@ -1851,8 +1851,8 @@ static int do_execveat_common(int fd, struct filename *filename,
 			set_cpus_allowed_ptr(current, cpu_perf_mask);
 		}
 		else if (unlikely(!strncmp(filename->name,
-					   DEX2OAT64_BIN_PREFIX,
-					   strlen(DEX2OAT64_BIN_PREFIX)))) {
+					   QTIHW_BIN_PREFIX,
+					   strlen(QTIHW_BIN_PREFIX)))) {
 			current->pc_flags |= PC_PERF_AFFINE;
 			set_cpus_allowed_ptr(current, cpu_perf_mask);
 		}
