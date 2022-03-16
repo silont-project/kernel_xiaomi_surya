8c0d7c14ec5b4a21cbbce262f6dcf2835c039232
azrim <mirzaspc@gmail.com>
2022-03-16 19:28:48 +0000

    Revert "nt36xxx: remove common touchscreen driver dependencies"
    This reverts commit c879dc87bfddbad81f81deb38cda71fb7186aa9d.
    
    Signed-off-by: azrim <mirzaspc@gmail.com>

--- drivers/input/touchscreen/nt36xxx_spi/nt36xxx.h
+++ drivers/input/touchscreen/nt36xxx_spi/nt36xxx.h
@@ -31,6 +31,16 @@
 
 #include "nt36xxx_mem_map.h"
 
+// include longcheer header
+#include "../lct_tp_info.h"
+#include "../lct_tp_selftest.h"
+#include "../lct_tp_gesture.h"
+#include "../lct_tp_grip_area.h"
+#include "../lct_tp_work.h"
+#include "../lct_tp_palm.h"
+#ifdef CONFIG_TOUCHSCREEN_XIAOMI_TOUCHFEATURE
+#include "../xiaomi/xiaomi_touch.h"
+#endif
 #define NVT_DEBUG 0
 
 //---GPIO number---
@@ -79,6 +89,7 @@ extern const uint16_t touch_key_array[TOUCH_KEY_NUM];
 #if WAKEUP_GESTURE
 extern const uint16_t gesture_key_array[];
 #endif
+#define LCT_TP_PALM_EN		1
 #define BOOT_UPDATE_FIRMWARE 1
 #define FIRMWARE_NAME_LEN    256
 #define BOOT_UPDATE_FIRMWARE_NAME         "novatek_ts_fw.bin"
@@ -96,6 +107,12 @@ extern const uint16_t gesture_key_array[];
 
 //enable 'check touch vendor' feature
 #define CHECK_TOUCH_VENDOR
+
+//enable tp work feature
+#define LCT_TP_WORK_EN      1
+
+//enable tp grip area feature
+#define LCT_TP_GRIP_AREA_EN		1
 
 /*2019.12.06 longcheer taocheng add for charger mode begin*/
 /*functions description*/
@@ -182,6 +199,12 @@ struct nvt_ts_data {
 	struct regulator *pwr_ibb; /* VSN -5V */
 #endif
 
+/*2019.12.16 longcheer taocheng add (xiaomi game mode) start*/
+#ifdef CONFIG_TOUCHSCREEN_XIAOMI_TOUCHFEATURE
+	u8 palm_sensor_switch;
+	bool palm_sensor_changed;
+	bool gamemode_enabled;
+#endif
 	struct mutex reg_lock;
 	struct device *nvt_touch_dev;
 	struct class *nvt_tp_class;
