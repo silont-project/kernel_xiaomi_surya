b3b2f47a6b25a3867347ab53e041d083b89aa4cc
azrim <mirzaspc@gmail.com>
2022-06-25 10:50:43 +0000

    fs/exec: match to our code
    Signed-off-by: azrim <mirzaspc@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1853,7 +1853,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 		else if (unlikely(!strncmp(filename->name,
 					   QTIHW_BIN_PREFIX,
 					   strlen(QTIHW_BIN_PREFIX)))) {
-			current->pc_flags |= PC_PERF_AFFINE;
+			current->flags |= PF_PERF_CRITICAL;
 			set_cpus_allowed_ptr(current, cpu_perf_mask);
 		}
 		else if (unlikely(!strcmp(filename->name, ZYGOTE32_BIN))) {
